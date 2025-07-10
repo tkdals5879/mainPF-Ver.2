6686afd12bfcf1c0545d81f5409e6044182bdfdb
tkdals5879 <tkdals58799@gmail.com>
2025-07-10 15:53:51 +0900

    about reDesign

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import Footer from '../component/Footer';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all'
